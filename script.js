3ffc2a9d8d79c1cb1032ad887567b740b03a27c7
52south <ravindudilshan55@yahoo.com>
2026-08-31 20:10:25 +1000

    Install Meta Pixel tracking

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Meta Pixel tracking for 52 South
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init','4099745546996426');
+fbq('track','PageView');
+
 const btn=document.querySelector('.menu-toggle');
 const nav=document.querySelector('.navlinks');
 if(btn){
