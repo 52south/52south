bef98b17e241b5d6bdfcc094833a7596d330bf5a
52south <ravindudilshan55@yahoo.com>
2026-08-31 20:37:32 +1000

    Track completed table bookings

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
 fbq('init','4099745546996426');
 fbq('track','PageView');
+if(location.pathname.replace(/\/+$/,'')==='/booking-confirmed'){fbq('track','Schedule',{content_name:'Table booking'});}
 
 const btn=document.querySelector('.menu-toggle');
 const nav=document.querySelector('.navlinks');
